b48d505b4cbf860bd9bedbb39dacee917e977083
Sam McCollum <smccollum@mccollum.enterprises>
2017-05-23 01:45:56 -0700

    Improve sample code

--- sample.c
+++ sample.c
@@ -11,22 +11,37 @@ void print_creds(){
 int main(int argc, char** argv){
 	wmappauth_init(); //initialize the login system (IMPORTANT!)
 	int code = -1;
+	/**
+	 * If this user hasn't signed in, ask them to login until they are successful (response 200 is the HTTP success response code, indicating login was successful)
+	 */
 	if(!load_credentials()){
 		while(code != 200){
 			code = terminal_login(); //prompt the user to login, which will save their token and signature in memory
 		}
 	}
-	//if you want to know what the variable code is, it's the HTTP status returned by the server
+	//if you want to know what the variable 'int code' is, it's the HTTP status returned by the server
 	print_creds();
+	/**
+	 * Check if the credentials are expired and, if so, ask them to login again
+	 */
 	if(credentials_are_expired()){
 		printf("credentials expired\n");
 		code = terminal_login();
 	}else{
 		printf("credentials not expired\n");
 	}
+	/**
+	 * Renew their token to keep them logged in (do this at least once every two-three weeks to keep it from expiring, but not more often than every couple of days)
+	 */
 	renew_token();
 	print_creds();
+	/**
+	 * Store their credentials to the disk so that they can be loaded next time
+	 */
 	persist_credentials();
+	/**
+	 * Deinitialize the library, prepare for end of program (or at least, end of use of wmappauth in program)
+	 */
 	wmappauth_deinit();
 	return 0;
 }
